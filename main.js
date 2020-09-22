3ef99904db69f9d32ba97bc47f1bb24c9f013777
Anup <10anupkalita@gmail.com>
2020-09-23 01:00:05 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$('.nav-bar a, .btn a').on('click', function(event){
+    if((this.hash!=='')){
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top -10
+      },
+      700
+      );
+    }
+  });
